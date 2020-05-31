64574ae2fe960134203867e1a871ae6fb23ba7da
Sam Robbins <samrobbinsgb@gmail.com>
2020-05-31 12:22:21 +0100

    We did it!

--- page-eater.js
+++ page-eater.js
@@ -1,22 +1,4 @@
-// alert(document.getElementById("span"));
-
-// This works for MDN
-// var elem = document.getElementsByClassName("highlight-span")[0];
-// elem.lastChild.data = "Sup nerds";
-//
-// var elem = document.getElementsByClassName("firstHeading")[0];
-// elem.firstChild.data = "Facebook is really fun";
-//
-// var i;
-// for (i = 0; i < document.getElementsByClassName("mw-headline").length; i++) {
-//   var elem = document.getElementsByClassName("mw-headline")[i];
-//   elem.firstChild.data = "Facebook is really fun";
-// }
-
-// var elem = document.getElementsByTagName("body")[0];
-// elem.style["background-color"] = "blue";
-// alert(window.location.href);
-if (window.location.href.startsWith("https://en.wikipedia.org"))
+if (window.location.href.startsWith("https://en.wikipedia.org")) {
   for (let j = 0; j < document.getElementsByTagName("*").length; j++) {
     var elem = document.getElementsByTagName("*")[j];
     elem.style["background-color"] =
@@ -27,10 +9,19 @@ if (window.location.href.startsWith("https://en.wikipedia.org"))
       "," +
       String(Math.floor(Math.random() * 255)) +
       ")";
-    console.log(elem);
   }
-// var elem = document.getElementsByClassName("mw-parser-output")[0];
-// elem.style["background-color"] = "orange";
-// for (item in document.getElementsByClassName("mw-headline")) {
-//   item.firstChild.data = "Facebook";
-// }
+}
+
+if (window.location.href.startsWith("https://stackoverflow.com")) {
+  var index_count = document.getElementsByClassName("question-summary").length;
+  var i = 0;
+  var delete_elems = setInterval(function () {
+    if (i >= index_count) {
+      clearInterval(delete_elems);
+    }
+
+    var elem = document.getElementsByClassName("question-summary")[i];
+    elem.style["opacity"] = 0;
+    i += 1;
+  }, 1000);
+}
